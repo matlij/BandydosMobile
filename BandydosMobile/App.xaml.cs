bdf957c1102bacec0f1a455f0dd0f6b8b537ccd5
Mattias Morell <dev@mattiasmorell.se>
2022-10-17 19:02:46 +0200

    Add project files.

---
+++ BandydosMobile/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace BandydosMobile;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ BandydosMobile/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BandydosMobile;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
